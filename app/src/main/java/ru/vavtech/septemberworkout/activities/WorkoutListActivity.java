5e9f90f438b9b798057dc8b56506148c0b58b9e5
Andrew <sit-surgut@mail.ru>
2018-10-10 21:54:00 +0500

    initGUI and parentActivityName

--- app/src/main/java/ru/vavtech/septemberworkout/activities/WorkoutListActivity.java
+++ app/src/main/java/ru/vavtech/septemberworkout/activities/WorkoutListActivity.java
@@ -1,15 +1,44 @@
 package ru.vavtech.septemberworkout.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import ru.vavtech.septemberworkout.R;
 
 public class WorkoutListActivity extends AppCompatActivity {
+    Button buttonPullingUp;
+    Button buttonSquat;
+    Button buttonBarbellBenchPress;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_list);
+
+        initGUI();
+    }
+
+    private void initGUI() {
+        buttonPullingUp = findViewById(R.id.button_pulling_up);
+        buttonSquat = findViewById(R.id.button_squat);
+        buttonBarbellBenchPress = findViewById(R.id.button_barbell_bench_press);
+
+        buttonPullingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(WorkoutListActivity.this,"Нажата кнопка "+getString(R.string.pulling_up),Toast.LENGTH_SHORT).show();
+                Intent startWorkoutDetailActivity = new Intent(WorkoutListActivity.this, WorkoutDetailActivity.class);
+                startActivity(startWorkoutDetailActivity);
+                
+            }
+        });
+
+
     }
 }
