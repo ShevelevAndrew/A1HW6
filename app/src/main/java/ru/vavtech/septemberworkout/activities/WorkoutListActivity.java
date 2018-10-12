2757af8c6e482d5ca47bcd0a0c58fef0c0ecc19a
Andrew <sit-surgut@mail.ru>
2018-10-12 23:40:47 +0500

    Добавление логирования, жизненого цикла приложения

--- app/src/main/java/ru/vavtech/septemberworkout/activities/WorkoutListActivity.java
+++ app/src/main/java/ru/vavtech/septemberworkout/activities/WorkoutListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 import ru.vavtech.septemberworkout.R;
 
 public class WorkoutListActivity extends AppCompatActivity {
+    public static final String TAG = "WorkoutListActivityLog" ;
     Button buttonPullingUp;
     Button buttonSquat;
     Button buttonBarbellBenchPress;
@@ -21,6 +23,42 @@ public class WorkoutListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_workout_list);
 
         initGUI();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG,"Вызван onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG,"Вызван onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG,"Вызван onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG,"Вызван onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG,"Вызван onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG,"Вызван onDestroy");
+        super.onDestroy();
     }
 
     private void initGUI() {
