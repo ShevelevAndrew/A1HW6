ad002c8305ebf1053fcb8f7cd957c19d3734887d
Andrew <sit-surgut@mail.ru>
2018-10-22 14:26:05 +0500

    Реализовано сохранение рекорда

--- app/src/main/java/ru/vavtech/septemberworkout/activities/WorkoutListActivity.java
+++ app/src/main/java/ru/vavtech/septemberworkout/activities/WorkoutListActivity.java
@@ -2,25 +2,19 @@ package ru.vavtech.septemberworkout.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
-import java.util.List;
-
-import ru.vavtech.septemberworkout.Model.Workout;
-import ru.vavtech.septemberworkout.Model.WorkoutList;
 import ru.vavtech.septemberworkout.R;
 import ru.vavtech.septemberworkout.list.WorkoutAdapter;
 
@@ -39,7 +33,6 @@ public class WorkoutListActivity extends AppCompatActivity {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(new WorkoutAdapter());
-
     }
 
 }
